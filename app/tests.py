cec233a6f755debb0725a4156e942951f3c68f0c
GuilhermeAngelo <guilhermeangelo2001@gmail.com>
2022-09-18 21:53:59 -0300

    implementando os testes

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Carro
+from django.db import models
+from django.urls import resolve,reverse
+from .views import create,update
 
-# Create your tests here.
+class TestModel(TestCase):
+
+    def test_should_create_carro(self):
+        carro = Carro.objects.create(nome='fiat',marca='fiat',cor='vermelho')
+        carro.save()
+
+        self.assertEqual(str(carro),'fiat')
+
+class testUrls(TestCase):
+    
+    def test_create_url_is_resolved(self):
+        url = reverse('create')
+
+        self.assertEquals(resolve(url).func, create)
+        self.assertEquals(resolve(url).route,'create/')
